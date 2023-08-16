56fcdd4cc97b7b0cdd469c7225fdf6a0129d8137
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-08-16 12:39:32 +0900

    Revert "タイトル　メニュー操作準備"
    This reverts commit c1d27df4c6fdacf40ca3ccf8bfe5bb1c21fe4f0c.

--- game02/Title.cpp
+++ game02/Title.cpp
@@ -2,24 +2,16 @@
 #include"Title.h"
 #include"GameMain.h"
 
-#define INPUT_ACCEPTANCE_TIME 60
-
 //-----------------------------------
 // コンストラクタ
 //-----------------------------------
 Title::Title()
 {
     flashing_time = 0;
-    input_time = 0;
-    cursor_y = 0;
-
+    push_time = 0;
     can_scene_change = FALSE;
-
-    select_menu = static_cast<int>(MENU::GAME_START); 
-    
     image = LoadGraph("images/home.png");
     imgae1 = LoadGraph("images/start.png");
-    cursor_image = LoadGraph("images/triangle.png");
 }
 
 //-----------------------------------
@@ -36,7 +28,7 @@ Title::~Title()
 //-----------------------------------
 void Title::Update(Key* key)
 {
-    input_time++;
+    push_time++;
 
     flashing_time++;
 
@@ -45,22 +37,9 @@ void Title::Update(Key* key)
         flashing_time = 0;
     }
 
-    if (key->KeyDown(B))
+    if (key->KeyDown(B) && push_time >= 30)
     {
         can_scene_change = TRUE;
-    }
-
-    //超高速でカーソルが移動しないようにする
-    if (++input_time > INPUT_ACCEPTANCE_TIME)
-    {
-        if (key->GetStickAngle(L).y > 0)
-        {
-            select_menu = (select_menu - 1 + static_cast<int>(MENU::MENU_SIZE)) % static_cast<int>(MENU::MENU_SIZE);
-        }
-        else if (key->GetStickAngle(L).y < 0)
-        {
-            select_menu = (select_menu + 1) % static_cast<int>(MENU::MENU_SIZE);
-        }
     }
 }
 
@@ -83,10 +62,8 @@ void Title::Draw() const
     DrawGraph(0, 0, image, false);
     DrawGraph(715, 250, imgae1, true);
 
-    DrawFormatString(100, 100, 0xFFFFFF, "%d ", select_menu);
-
-   // //カーソル位置
-    //DrawTriangle(745, 280, 700, 240, 700, 320, GetColor(250, 0, 0), TRUE);
+    //カーソル位置
+    DrawTriangle(745, 280, 700, 240, 700, 320, GetColor(250, 0, 0), TRUE);
 }
 
 
