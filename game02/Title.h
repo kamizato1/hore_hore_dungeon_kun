8eccff23218ab75941a6bdee941244cfcb58fa56
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-08-16 12:10:13 +0900

    Revert "タイトル　メニュー操作準備"
    This reverts commit c1d27df4c6fdacf40ca3ccf8bfe5bb1c21fe4f0c.

--- game02/Title.h
+++ game02/Title.h
@@ -4,7 +4,6 @@
 #include"Key.h"
 #include"define.h"
 
-
 class Title : public AbstractScene
 {
 private:
@@ -12,26 +11,11 @@ private:
     //テスト
     int image;
     int imgae1;
-    int cursor_image; 
-    int cursor_y;
     //テスト
 
     int flashing_time; //文字点滅時間
-    int input_time; // 入力受付時間
-    int select_menu; // 選択しているメニュー
-
+    int push_time; // ゲームメインに行けるようになるまでの時間
     bool can_scene_change; //次のシーンに行けるか
-
-    //連番機能がついた構造体
-    enum class MENU
-    {
-        GAME_START,
-        HELP,
-        RANKING,
-        CLEDIT,
-        END,
-        MENU_SIZE
-    };
 
 
 public:
