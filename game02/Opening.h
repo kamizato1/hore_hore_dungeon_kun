7ac3e8d58802a36f5552671658adf789a9713fe4
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2023-08-28 12:08:51 +0900

    タイトル画面ある程度完成（残り遷移のみ）

--- game02/Opening.h
+++ game02/Opening.h
@@ -3,6 +3,36 @@
 class Opening :
     public AbstractScene
 {
+private:
+
+    //アニメーション
+    int animation;
+
+    //アニメーション時間
+    int animation_time;
+
+    //画像   
+    int image[4];
+
+    //次のシーンに行けるのか
+    bool can_scene_change;
+
+public:
+
+    //コンストラクタ
+    Opening();
+
+    ////デストラクタ
+    ~Opening();
+
+    //描画以外の更新を実装する
+    void Update(Key* key) override;
+
+    //描画に関することを実装する
+    void Draw() const override;
+
+    //シーン切り替え
+    AbstractScene* ChangeScene() override;
 
 };
 
--- game02/Title.cpp
+++ game02/Title.cpp
@@ -2,7 +2,7 @@
 #include"Title.h"
 #include"GameMain.h"
 
-#define  INPUT_ACCEPTANCE_TIME 60
+#define  INPUT_ACCEPTANCE_TIME 10
 
 //-----------------------------------
 // コンストラクタ
@@ -11,8 +11,53 @@ Title::Title()
 {
 	input_time = 0;
 	cursor_y = 0;
-	select_menu = static_cast<int>(MENU::GAME_START);
+	select_menu = static_cast<int>(MENU_NAME::GAME_START);
 	can_scene_change = FALSE;
+
+	image = LoadGraph("images/home.png");
+
+	//メニュー画像 (選択されている状態)
+	mene[0].image = LoadGraph("images/Menu/start.png");
+	mene[0].x = 1010;
+	mene[0].y = 250;
+
+	mene[1].image = LoadGraph("images/Menu/help.png");
+	mene[1].x = 1010;
+	mene[1].y = 350;
+
+	mene[2].image = LoadGraph("images/Menu/ranking.png");
+	mene[2].x = 1010;
+	mene[2].y = 450;
+
+	mene[3].image = LoadGraph("images/Menu/cledit.png");
+	mene[3].x = 1010;
+	mene[3].y = 550;
+
+	mene[4].image = LoadGraph("images/Menu/end.png");
+	mene[4].x = 1010;
+	mene[4].y = 650;
+
+	//メニュー画像（選択されていない状態）
+	mene[5].image = LoadGraph("images/Menu/start2.png");
+	mene[5].x = 1010;
+	mene[5].y = 250;
+
+	mene[6].image = LoadGraph("images/Menu/help2.png");
+	mene[6].x = 1010;
+	mene[6].y = 350;
+
+	mene[7].image = LoadGraph("images/Menu/ranking2.png");
+	mene[7].x = 1010;
+	mene[7].y = 450;
+
+	mene[8].image = LoadGraph("images/Menu/cledit2.png");
+	mene[8].x = 1010;
+	mene[8].y = 550;
+
+	mene[9].image = LoadGraph("images/Menu/end2.png");
+	mene[9].x = 1010;
+	mene[9].y = 650;
+
 }
 
 //-----------------------------------
@@ -29,21 +74,24 @@ Title::~Title()
 //-----------------------------------
 void Title::Update(Key* key)
 {
-
+	//ボタンが押されたら
 	if (key->KeyDown(B))
 	{
 		can_scene_change = TRUE;
 	}
 
+	//入力受付るのか
 	if (++input_time > INPUT_ACCEPTANCE_TIME)
 	{
 		if (key->GetStickAngle(L).y > 0)
 		{
-			select_menu = (select_menu + 1) % static_cast<int>(MENU::MENU_SIZE);
+			select_menu = (select_menu + 1) % static_cast<int>(MENU_NAME::MENU_SIZE);
+			input_time = 0;
 		}
 		else if (key->GetStickAngle(L).y < 0)
 		{
-			select_menu = (select_menu - 1 + static_cast<int>(MENU::MENU_SIZE)) % static_cast<int>(MENU::MENU_SIZE);
+			select_menu = (select_menu - 1 + static_cast<int>(MENU_NAME::MENU_SIZE)) % static_cast<int>(MENU_NAME::MENU_SIZE);
+			input_time = 0;
 		}
 	}
 
@@ -55,11 +103,37 @@ void Title::Update(Key* key)
 //-----------------------------------
 void Title::Draw() const
 {
-	SetFontSize(30);
+	//背景画像
+	DrawGraph(0, 0, image, false);
 
-	DrawString(190, 200, "惚れ掘れダンジョン君！", 0xffffff);
+	//通常状態の画像
+	for (int  i = 5; i < 10; i++)
+	{
+		DrawRotaGraph(mene[i].x, mene[i].y, 0.9f, 0, mene[i].image, TRUE);
+	}
 
-	DrawString(190, 300, "Bボタンを押そう！", 0xffffff);
+	//選択しているメニュー画像（赤い）を表にだす。
+	switch (select_menu)
+	{
+	case 0:
+		DrawRotaGraph(mene[0].x, mene[0].y, 0.9f, 0, mene[0].image, TRUE);
+		break;
+	case 1:
+		DrawRotaGraph(mene[1].x, mene[1].y, 0.9f, 0, mene[1].image, TRUE);
+		break;
+	case 2:
+		DrawRotaGraph(mene[2].x, mene[2].y, 0.9f, 0, mene[2].image, TRUE);
+		break;
+
+	case 3:
+		DrawRotaGraph(mene[3].x, mene[3].y, 0.9f, 0, mene[3].image, TRUE);
+		break;
+
+	case 4:
+		DrawRotaGraph(mene[4].x, mene[4].y, 0.9f, 0, mene[4].image, TRUE);
+		break;
+	}
+
 }
 
 
@@ -68,10 +142,29 @@ void Title::Draw() const
 //-----------------------------------
 AbstractScene* Title::ChangeScene()
 {
-
+	//ボタンが押されたら
 	if (can_scene_change)
 	{
-		return new GameMain(); //次の遷移
+		switch (select_menu)
+		{
+		case 0: //ゲームメインに遷移
+			return new GameMain();
+			break;
+		case 1:
+			//return new herupu
+			break;
+		case 2:
+			//return new rannking
+			break;
+
+		case 3:
+			//return new クレジット
+			break;
+
+		case 4: //ゲーム終了
+			return nullptr;
+			break;
+		}
 	}
 
 	return this;
