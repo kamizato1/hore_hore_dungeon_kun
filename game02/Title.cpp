ff161200ef54110b9cd2011c41430acc4af7412f
kamizato1 <106945305+kamizato1@users.noreply.github.com>
2023-09-05 14:39:59 +0900

    fffff

--- game02/Title.cpp
+++ game02/Title.cpp
@@ -11,54 +11,16 @@
 Title::Title()
 {
 	input_time = 0;
-	cursor_y = 0;
-	select_menu = static_cast<int>(MENU_NAME::GAME_START);
 	can_scene_change = FALSE;
-
+	select_menu = 0;
 	image = LoadGraph("images/home.png");
-
-	//メニュー画像 (選択されている状態)
-	mene[0].image = LoadGraph("images/Menu/start.png");
-	mene[0].x = 1010;
-	mene[0].y = 250;
-
-	mene[1].image = LoadGraph("images/Menu/help.png");
-	mene[1].x = 1010;
-	mene[1].y = 350;
-
-	mene[2].image = LoadGraph("images/Menu/ranking.png");
-	mene[2].x = 1010;
-	mene[2].y = 450;
-
-	mene[3].image = LoadGraph("images/Menu/cledit.png");
-	mene[3].x = 1010;
-	mene[3].y = 550;
-
-	mene[4].image = LoadGraph("images/Menu/end.png");
-	mene[4].x = 1010;
-	mene[4].y = 650;
-
-	//メニュー画像（選択されていない状態）
-	mene[5].image = LoadGraph("images/Menu/start2.png");
-	mene[5].x = 1010;
-	mene[5].y = 250;
-
-	mene[6].image = LoadGraph("images/Menu/help2.png");
-	mene[6].x = 1010;
-	mene[6].y = 350;
-
-	mene[7].image = LoadGraph("images/Menu/ranking2.png");
-	mene[7].x = 1010;
-	mene[7].y = 450;
-
-	mene[8].image = LoadGraph("images/Menu/cledit2.png");
-	mene[8].x = 1010;
-	mene[8].y = 550;
-
-	mene[9].image = LoadGraph("images/Menu/end2.png");
-	mene[9].x = 1010;
-	mene[9].y = 650;
-
+	int menu_image[8];
+	LoadDivGraph("images/Menu/menu.png", 8, 2, 4, 600, 100, menu_image);
+	int count = 0;
+	for (int i = 0; i < MENU_NUM; i++)
+	{
+		for (int j = 0; j < 2; j++)this->menu_image[i][j] = menu_image[count++];
+	}
 }
 
 //-----------------------------------
@@ -69,35 +31,30 @@ Title::~Title()
 
 }
 
-
 //-----------------------------------
 // 更新
 //-----------------------------------
 void Title::Update(Key* key)
 {
 	//ボタンが押されたら
-	if (key->KeyDown(B))
-	{
-		can_scene_change = TRUE;
-	}
+	if (key->KeyDown(B))can_scene_change = TRUE;
 
 	//入力受付るのか
 	if (++input_time > INPUT_ACCEPTANCE_TIME)
 	{
 		if (key->GetStickAngle(L).y > 0)
 		{
-			select_menu = (select_menu + 1) % static_cast<int>(MENU_NAME::MENU_SIZE);
+			if (++select_menu == MENU_NUM)select_menu = 0;
 			input_time = 0;
 		}
 		else if (key->GetStickAngle(L).y < 0)
 		{
-			select_menu = (select_menu - 1 + static_cast<int>(MENU_NAME::MENU_SIZE)) % static_cast<int>(MENU_NAME::MENU_SIZE);
+			if (--select_menu < 0)select_menu = MENU_NUM - 1;
 			input_time = 0;
 		}
 	}
 
 }
-
 
 //-----------------------------------
 // 描画
@@ -107,36 +64,14 @@ void Title::Draw() const
 	//背景画像
 	DrawGraph(0, 0, image, false);
 
-	//通常状態の画像
-	for (int  i = 5; i < 10; i++)
+	for (int i = 0; i < MENU_NUM; i++)
 	{
-		DrawRotaGraph(mene[i].x, mene[i].y, 0.9f, 0, mene[i].image, TRUE);
+		bool flg = FALSE;
+		if (i == select_menu)flg = TRUE;
+
+		DrawRotaGraph(1010, 250 + (i * 100), 1, 0, menu_image[i][flg], TRUE);
 	}
-
-	//選択しているメニュー画像（赤い）を表にだす。
-	switch (select_menu)
-	{
-	case 0:
-		DrawRotaGraph(mene[0].x, mene[0].y, 0.9f, 0, mene[0].image, TRUE);
-		break;
-	case 1:
-		DrawRotaGraph(mene[1].x, mene[1].y, 0.9f, 0, mene[1].image, TRUE);
-		break;
-	case 2:
-		DrawRotaGraph(mene[2].x, mene[2].y, 0.9f, 0, mene[2].image, TRUE);
-		break;
-
-	case 3:
-		DrawRotaGraph(mene[3].x, mene[3].y, 0.9f, 0, mene[3].image, TRUE);
-		break;
-
-	case 4:
-		DrawRotaGraph(mene[4].x, mene[4].y, 0.9f, 0, mene[4].image, TRUE);
-		break;
-	}
-
 }
-
 
 //-----------------------------------
 // 遷移
--- game02/Title.h
+++ game02/Title.h
@@ -4,40 +4,18 @@
 #include"Key.h"
 #include"define.h"
 
+#define MENU_NUM 4
+
 class Title : public AbstractScene
 {
 private:
 
     int image; //背景画像
+    int menu_image[MENU_NUM][2]; //メニュー画像
 
-    int cursor_image;  //カーソル画像
-    int menu_image[10]; //メニュー画像
-    int cursor_y; //カーソル位置
     int input_time; //入力受付時間
     int select_menu; // 現在入力しているメニュー
     bool can_scene_change; //次のシーンに行けるか
-    
-
-    enum class MENU_NAME
-    {
-        GAME_START,
-        HELP,
-        RANKING,
-        CLEDIT,
-        END,
-        MENU_SIZE
-    };
-
-
-    struct MENE
-    {
-        int image;
-        int x;
-        int y;
-    };
-
-
-    MENE mene[10];
 
 public:
 
